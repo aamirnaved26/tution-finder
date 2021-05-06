90d225583753f4b231159cdd5c86d7b5778d3e17
Aamir Naved <83628306+aamirnaved26@users.noreply.github.com>
2021-05-06 12:20:11 +0530

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+jQuery.noConflict();
+jQuery(document).ready(function ($) {
+    //   $("p").click(function () {
+    //     alert("Element Selector");
+    //   });
+
+    //   $("#btn-id").click(function () {
+    //     alert("ID Selector");
+    //   });
+
+    //   $(".btn-class").click(function () {
+    //     alert("Class Selector");
+    //   });
+
+
+   
+
+    // $("#signupId").click(function (e) {
+    //     console.log("Form Submitted");
+    //     e.preventDefault();
+    // });
+
+
+});
